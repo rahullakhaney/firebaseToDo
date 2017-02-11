6f2c7fb0150a017fad8093105f8dde7db13f9fea
rahullakhaney <rahul@codebrew.io>
2017-02-11 17:21:12 +0400

    Unified IOS and Android Dev in App

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Stylesheet
+} from 'react-native'
+
+export default class App extends Component {
+    render() {
+      return (
+          <Text>HelloWorld</Text>
+      )
+    }
+}
