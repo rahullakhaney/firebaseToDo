3ee98ec857c1142a3ff03a9e00de591bc1eb06cf
rahullakhaney <rahul@codebrew.io>
2017-02-11 18:08:03 +0400

    Added firebase

--- app/App.js
+++ app/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react'
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TouchableHighlight,
+  TextInput,
+  ListView
 } from 'react-native'
 
 import ViewContainer from './components/ViewContainer'
 import StatusbarBackground from './components/StatusbarBackground'
+import * as firebase from "firebase";
+
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    var firebaseRef = firebase.initializeApp({
+      apiKey: 'AIzaSyCjgZBoyoSTk3huVEl3JnVTwKzgT2uPS1Y',
+      authDomain: 'fir-todo-a6a6e.firebaseapp.com',
+      databaseURL: 'https://fir-todo-a6a6e.firebaseio.com'
+});
+  }
     render() {
       return (
           <ViewContainer>
           <StatusbarBackground />
-            <Text>
-            Hello World
-            </Text>
+
           </ViewContainer>
       )
     }
